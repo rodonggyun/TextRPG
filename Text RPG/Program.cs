e6b3d2c35ec049de5094a2f6149d95511fc23295
nolabong1 <noano1@naver.com>
2025-07-14 16:48:11 +0900

    마을방문 순서수정

--- Text RPG/Program.cs
+++ Text RPG/Program.cs
@@ -15,18 +15,18 @@ class Program
         {
             int selected = MenuSelector.Select("무엇을 하시겠습니까?", new List<string>
             {
-                "스탯 확인", "저장/불러오기", "게임 종료", "마을 방문"
+                "스탯 확인", "저장/불러오기", "마을 방문", "게임 종료" 
             });
 
             Console.Clear();
             if (selected == 0) player.DisplayStats();
             else if (selected == 1) SaveLoadMenu(ref player);
-            else if (selected == 2) break;
-            else if (selected == 3)
+            else if (selected == 2)
             {
                 Town town = new Town();
                 town.Enter(player);
             }
+            else if (selected == 3) break;
             Console.WriteLine("\n아무 키나 눌러 계속...");
             Console.ReadKey();
         }
