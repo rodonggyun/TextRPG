59ba29be8d4a9a8a56a8d5bd2d14cb8753abad27
myeoungeun <melee0110@naver.com>
2025-07-14 16:58:12 +0900

    dlr

---
+++ Text RPG/Dungeon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_RPG
+{
+    class MosterInf { //몬스터 정보
+        public string name;
+        public int hp;
+        public int level;
+        public int exp;
+        public bool dead; //몬스터 사망 유무
+        public int gold;
+        public int atk; //몬스터 공격
+    }
+
+    internal class Dungeon
+    {
+
+    }
+
+    public void Monster() {
+        
+    }
+
+
+}
