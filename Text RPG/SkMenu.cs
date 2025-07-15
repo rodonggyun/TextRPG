2802c8fc2e21ac7b7fe00f16d6be905fd6d6a407
myeoungeun <melee0110@naver.com>
2025-07-15 19:10:18 +0900

    보스, 스킬공격

--- Text RPG/SkMenu.cs
+++ Text RPG/SkMenu.cs
@@ -11,22 +11,44 @@ namespace Text_RPG
     {
         public void UseSkillMenu(Player player, MonsterInfo enemy)
         {
-            if (player.Skills.Count == 0)
+            List<Skill> availableSkills = new List<Skill>(); //스킬 사용이 가능한 스킬들 모은 리스트
+
+            foreach (Skill s in player.Skills) //플레이어 스킬들 순차대입
             {
-                Console.WriteLine("아직 배운 스킬이 없습니다.");
-                return;
+                if (s != null) 
+                {
+                    // 나중에 쿨타임, 잠김 조건 등도 여기에 추가 가능
+                    availableSkills.Add(s); //리스트에 사용가능한 스킬 추가
+                }
+            }
+            if (availableSkills.Count == 0) //스킬이 하나도 없다면
+            {
+                Console.WriteLine("\n아직 배운 스킬이 없습니다.\n");
             }
 
-            List<string> skillNames = new List<string>();
-            foreach (var s in player.Skills)
+            List<string> skillNames = new List<string>(); //스킬 보여줄 수 있도록 만든 str 리스트
+            foreach (var s in availableSkills) //스킬 이름들 추가
+            {
                 skillNames.Add($"{s.Name} ({s.Description})");
+            }
 
-            int selection = MenuSelector.Select("사용할 스킬을 선택하세요!", skillNames, true);
+            skillNames.Add("일반 공격 선택");
+
+            int selection = ShowMenu.ShowMenus(skillNames);
             if (selection >= 0)
             {
-                var skill = player.Skills[selection];
-                //skill.Effect(player, enemy);
-                // 쿨다운 처리 추가 가능
+                if (selection == availableSkills.Count)
+                { //마지막은 일반 공격이니깐
+                    int damage = player.AttackPower;
+                    enemy.hp -= damage;
+                    Console.WriteLine($"\n▶ 일반 공격! {enemy.name}에게 {damage}의 피해를 입혔습니다!");
+                }
+                else {
+                    Console.WriteLine("사용할 스킬을 선택하세요!\n");
+                    Skill selectedSkill = availableSkills[selection]; //선택한 스킬
+                    Console.WriteLine($"\n▶ {selectedSkill.Name} 스킬을 사용합니다!");
+                    //선택한 스킬 효과 적용할 코드 필요함
+                }
             }
         }
     }
